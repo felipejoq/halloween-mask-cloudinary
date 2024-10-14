3dc6b6b574e57cb4118a8f8bed58de9c901d63e2
felipejoq <felipejofreq@gmail.com>
2024-10-14 19:16:28 -0300

    genera 5 imagenes

--- src/uploads/utils/mask.array.js
+++ src/uploads/utils/mask.array.js
@@ -1,7 +1,7 @@
 export const masks = [
   {public_id: 'masks:mask_1', width: "1.5"},
-  // {public_id: 'masks:mask_2', width: "1.5"},
-  // {public_id: 'masks:mask_3', width: "1.5"},
-  // {public_id: 'masks:mask_4', width: "1.3"},
+  {public_id: 'masks:mask_2', width: "1.5"},
+  {public_id: 'masks:mask_3', width: "1.5"},
+  {public_id: 'masks:mask_4', width: "1.3"},
   {public_id: 'masks:mask_5', width: "1.3"},
 ];
