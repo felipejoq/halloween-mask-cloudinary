83cc71169b652f1a73b91561d2e95fb9686690b1
felipejoq <felipejofreq@gmail.com>
2024-10-15 14:47:19 -0300

    cambia límite de imagen a 10 mb

--- src/Server.js
+++ src/Server.js
@@ -21,11 +21,11 @@ export class Server {
 
     // File upload middleware configuration
     this.app.use(fileUpload({
-      limits: { fileSize: 5 * 1024 * 1024 },
+      limits: { fileSize: 10 * 1024 * 1024 },
       abortOnLimit: true,
       useTempFiles: true,
       tempFileDir: '/tmp/',
-      responseOnLimit: `El archivo excede el tamaño permitido 5MB bytes`
+      responseOnLimit: `El archivo excede el tamaño permitido 10MB bytes`
     }))
 
     // * Public folders
